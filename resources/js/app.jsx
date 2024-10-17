957fedbce2574561cab105064290b47ca4df9a15
M Isa Al Anshori <isabungsu@gmail.com>
2024-10-17 16:27:00 +0700

    Frontend Layouting Submit Dokumen Page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,17 +2,40 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { pdfjs } from 'react-pdf';
+import { SnackbarProvider, closeSnackbar } from 'notistack';
+import { Button, createTheme, CssBaseline } from '@mui/material';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
 
 createInertiaApp({
     // Get all React components from resources/js/pages folder
     resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/**/*.jsx')),
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+        const theme = createTheme();
+        createRoot(el).render(
+            <>
+                <CssBaseline />
+                <SnackbarProvider
+                    action={(snackbarId) => (
+                        <Button color='white' onClick={() => closeSnackbar(snackbarId)}>
+                            OK
+                        </Button>
+                    )}
+                >
+                    <App {...props} />
+                </SnackbarProvider>
+            </>
+
+
+        )
     },
 })
